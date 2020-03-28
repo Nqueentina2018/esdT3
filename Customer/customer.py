085992f17cfb1692709942e6f242cbb9a0fdfe51
Tiffany Tan Min <tiffanytan.2018@sis.smu.edu.sg>
2020-03-28 14:35:59 +0800

    Clean up and finalized customer.py

--- Customer/customer.py
+++ Customer/customer.py
@@ -1,14 +1,9 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
-from os import environ
 app = Flask(__name__)
 
-# connect this file to wamp
-# root@localhost:3306/book is to login and refering to specific database
-# root:abc -> username:password
-app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('dbURL')
-# 'mysql+mysqlconnector://root@localhost:3306/book'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root@localhost:3306/customer'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
@@ -16,21 +11,47 @@ CORS(app)
 
 class Customer(db.Model):
     __tablename__ = 'customer'
+    cid = db.Column(db.Integer , primary_key=True)
+    name = db.Column(db.String(15) , nullable =False)
+    phone = db.Column(db.String(10) , nullable = False)
+    ewallet = db.Column(db.Float(precision=2))
  
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(15), nullable=False)
-    phone = db.Column(db.String(10), nullable=False)
-    wallet = db.Column(db.Double(Precision=2))
- 
-    def __init__(self, id , name , phone , wallet):
-        self.id = id
+    def __init__(self, cid , name , phone , ewallet):
+        self.cid = cid
         self.name = name
         self.phone = phone
-        self.wallet = wallet
+        self.ewallet = ewallet
  
     def json(self):
-        return {"id": self.id, "name": self.name, "phone": self.phone, "wallet": self.wallet}
+        return {"id": self.cid, "name": self.name, "phone": self.phone, "ewallet": self.ewallet}
+    def jsonewallet(self):
+        return {"ewallet": self.ewallet}
+
 
 @app.route("/customer")
 def get_all():
-    return jsonify({"customer": [customer.json() for book in Customer.query.all()]})
+    return jsonify({"customers": [customer.json() for customer in Customer.query.all()]})
+
+
+@app.route("/customer" , methods = ['POST'] )
+def find_by_cid():
+    data = request.get_json()
+    cid= str(data['cid'])
+    if (Customer.query.filter_by(cid=cid).first()):
+        customer = Customer.query.filter_by(cid=cid).first()
+        return jsonify(customer.json())
+
+    return jsonify({"message": "Customer not found."}), 404
+
+@app.route("/customer/getewallet" , methods=['POST'])
+def ewallet_by_cid():
+    data = request.get_json()
+    cid= str(data['cid'])
+    if (Customer.query.filter_by(cid=cid).first()):
+        customer = Customer.query.filter_by(cid=cid).first()
+        return jsonify(customer.jsonewallet())
+
+    return jsonify({"message": "Customer not found."}), 404
+
+if __name__ == '__main__':
+    app.run()
