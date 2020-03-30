a6a11829c8dbccf27f53fa90f512e8196ba3a11b
shourya-goenka <54571965+shourya-goenka@users.noreply.github.com>
2020-03-30 19:41:25 +0800

    Order files

---
+++ Order/order.py
@@ -0,0 +1,64 @@
+# Import Flask and initialize a Flask application
+# Also imported request &  jsonify that will be used later
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+ 
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root@localhost:3306/order'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+ 
+db = SQLAlchemy(app)
+CORS(app)
+
+class Order(db.Model):
+    __tablename__ = 'order'
+    orderid = db.Column(db.Integer , primary_key=True)
+    storeidid = db.Column(db.Integer , nullable=False)
+    status = db.Column(db.String(15) , nullable =False)
+    price = db.Column(db.Float(precision=2) , nullable=False)
+ 
+    def __init__(self, orderid , storeid, status, price):
+        self.orderid = orderid
+        self.storeid = storeid
+        self.status = status
+        self.price = price
+ 
+    def json(self):
+        return {"orderid": self.orderid, "storeid": self.storeid, "status": self.status, "price": self.price}
+    
+@app.route("/order")
+def get_all():
+    return jsonify({"orders": [order.json() for order in Order.query.all()]})
+
+@app.route("/order" , methods = ['POST'] )
+def find_by_orderid():
+    data = request.get_json()
+    orderid= str(data['orderid'])
+    if (Order.query.filter_by(orderid=orderid).first()):
+        order = Order.query.filter_by(orderid=orderid).first()
+        return jsonify(order.json())
+
+    return jsonify({"message": "Order not found."}), 404
+
+@app.route("/neworder" , methods = ['POST'] )
+def add_new_order():
+    data = request.get_json()
+    storeid= str(data['orderid'])
+    status= str(data['status'])
+    price= str(data['price'])
+    query = "INSERT INTO order(storeid, status, price) " \      
+            "VALUES(%s,%s, %s)"
+
+@app.route("/updateorder" , methods = ['POST'] )
+def update_order():
+    data = request.get_json()
+    orderid= str(data['orderid'])
+    status= str(date['status'])
+    if (Order.query.filter_by(orderid=orderid).first()):
+        order = Order.query.filter_by(orderid=orderid).first()
+        
+    return jsonify({"message": "Order not found."}), 404        
+
+if __name__ == '__main__':
+    app.run()
