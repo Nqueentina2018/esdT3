cd9d80e0fefdf5ec8c7cc43a26e9a14df15ea117
shourya-goenka <54571965+shourya-goenka@users.noreply.github.com>
2020-03-30 19:59:31 +0800

    Order files

--- Order/order.py
+++ Order/order.py
@@ -1,8 +1,12 @@
 # Import Flask and initialize a Flask application
 # Also imported request &  jsonify that will be used later
+# Import MySQLConnection to connect to database
+# Also imported Error that will be used later
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+from mysql.connector import MySQLConnection, Error
+from python_mysql_dbconfig import read_db_config
  
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root@localhost:3306/order'
@@ -49,16 +53,42 @@ def add_new_order():
     price= str(data['price'])
     query = "INSERT INTO order(storeid, status, price) " \      
             "VALUES(%s,%s, %s)"
+    args = (storeid, status, price)
+    try:
+        db_config = read_db_config()
+        conn = MySQLConnection(**db_config)
+ 
+        cursor = conn.cursor()
+        cursor.execute(query, args)
+    conn.commit()
+    except Error as error:
+        print(error)
+ 
+    finally:
+        cursor.close()
+        conn.close() 
 
 @app.route("/updateorder" , methods = ['POST'] )
 def update_order():
     data = request.get_json()
     orderid= str(data['orderid'])
     status= str(date['status'])
-    if (Order.query.filter_by(orderid=orderid).first()):
-        order = Order.query.filter_by(orderid=orderid).first()
-        
-    return jsonify({"message": "Order not found."}), 404        
+    query= "UPDATE order SET status = status WHERE orderid = order id"\
+           "VALUES(%s,%s)"
+    args = (staus, orderid)
+    try:
+        db_config = read_db_config()
+        conn = MySQLConnection(**db_config)
+ 
+        cursor = conn.cursor()
+        cursor.execute(query, args)
+    conn.commit()
+    except Error as error:
+        print(error)
+ 
+    finally:
+        cursor.close()
+        conn.close() 
 
 if __name__ == '__main__':
     app.run()
