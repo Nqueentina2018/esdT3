777fca0d40740e79877ee03c2daf453fca7a6dda
Nqueentina2018 <54776842+Nqueentina2018@users.noreply.github.com>
2020-03-30 18:08:54 +0800

    Update payment.py

--- Payment/payment.py
+++ Payment/payment.py
@@ -8,7 +8,8 @@ app = Flask(__name__)
 import os
 import stripe
 import requests
-customerURL = "http://localhost:5002/customer"
+customerURL = "http://localhost:5002/customer
+ewalletURL = "http://localhost:5002/customer/getewallet"
 
 pub_key = 'pk_test_a4WmBvNzgsdxl168Wwu0aGde00kaznh0SL'
 secret_key = 'sk_test_n2Zbe1bhsIhVn7XCpYSIBSK600OHvj08YF'
@@ -33,6 +34,7 @@ def topuppayment():
 
     return requests.post(customerURL, json=charge)
 
+
 @app.route('/successtopup')
 def successtopup():
     return render_template('successtopup.html')
@@ -50,7 +52,34 @@ def receiveOrder():
         print(order)
         replymessage = json.dumps({"message": "Order should be in JSON", "data": order}, default=str)
         return replymessage, 400 # Bad Request
-    
+
+    result = processPayment(order)
+
+    if result['status']:
+        return 200
+    else:
+        return 501
+
+def processPayment(order):
+
+    #Get the e balance amount
+    ewallet = request.get(ewalletURL)
+
+    #Get the order total amount
+    orderAmount = order['total']
+
+    #Check if the balance is enough to pay
+    if ewallet < orderAmount:
+        resultstatus = 501
+        messagestatus = "Ewallet amount is not enough. Please top-up."
+
+    else:
+        newEwalletBalance = ewallet - orderAmount
+        resultstatus = 200
+        messagestatus = "Payment Successful!"
+
+    result = {'status': resultstatus, 'message' : messagestatus}
+    return result 
 
 
 if __name__ == '__main__':
