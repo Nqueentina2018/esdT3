e542b42a75e288596b6e905f48524c1cbf26ba21
Nqueentina2018 <54776842+Nqueentina2018@users.noreply.github.com>
2020-03-31 21:15:57 +0800

    update topupAmt cents to dollar

--- Payment/payment.py
+++ Payment/payment.py
@@ -52,7 +52,7 @@ def topuppayment():
     #2. Retrive topup amount from charge
     cid = customerDetailsfromUI['cid']
     currentEwalletBalance = customerDetailsfromUI['eWallet']     
-    topupAmt = charge['amount']
+    topupAmt = int(str(charge['amount'])[:-2])
 
     #Add the topup amount to current ewallet balance
     newEwalletBalance = currentEwalletBalance + topupAmt
