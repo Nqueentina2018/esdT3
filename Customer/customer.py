026500245c102a8381c556224cf1adf8e79c867d
Tiffany Tan Min <tiffanytan.2018@sis.smu.edu.sg>
2020-03-31 20:55:04 +0800

    Added authentication for customer.py

--- Customer/customer.py
+++ Customer/customer.py
@@ -14,23 +14,41 @@ class Customer(db.Model):
     cid = db.Column(db.Integer , primary_key=True)
     name = db.Column(db.String(15) , nullable =False)
     phone = db.Column(db.String(10) , nullable = False)
+    username = db.Column(db.String(30) , nullable =False)
+    password = db.Column(db.String(30) , nullable = False)
     ewallet = db.Column(db.Float(precision=2))
  
-    def __init__(self, cid , name , phone , ewallet):
+    def __init__(self, cid , name , phone , ewallet , username , password):
         self.cid = cid
         self.name = name
         self.phone = phone
         self.ewallet = ewallet
+        self.username = username
+        self.password = password
  
     def json(self):
-        return {"id": self.cid, "name": self.name, "phone": self.phone, "ewallet": self.ewallet}
+        return {"id": self.cid, "name": self.name, "phone": self.phone, "ewallet": self.ewallet , "username": self.username, "password": self.password}
     def jsonewallet(self):
         return {"ewallet": self.ewallet}
+    def jsonui(self):
+        return {"id": self.cid, "name": self.name, "phone": self.phone, "ewallet": self.ewallet , "username": self.username}
 
 
 @app.route("/customer")
 def get_all():
     return jsonify({"customers": [customer.json() for customer in Customer.query.all()]})
+
+@app.route("/customer/auth" , methods = ['POST'])
+def auth():
+    data = request.get_json()
+    username = data['username']
+    if (Customer.query.filter_by(username=username).first()):
+        customer = Customer.query.filter_by(username=username).first()
+        if customer.password == data['password']:
+            return jsonify(customer.jsonui())
+        else :
+            return jsonify({"message": "password is incorrect."}), 404
+    return jsonify({"message": "username is incorrect"}),404
 
 
 @app.route("/customer" , methods = ['POST'] )
